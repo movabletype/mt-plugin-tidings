4c9ee821ba05984ddeddad3288219e1bd7324739
Finn Smith <finn@timeghost.net>
2010-03-19 18:23:53 -0400

    Clean up copright, license, and support notices.

--- plugins/Tidings/lib/Tidings/Util.pm
+++ plugins/Tidings/lib/Tidings/Util.pm
@@ -1,15 +1,15 @@
-#############################################################################
+##############################################################################
 # Copyright © 2009 Six Apart Ltd.
-# This program is free software: you can redistribute it and/or modify it 
+# This program is free software: you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License as published
-# by the Free Software Foundation, or (at your option) any later version.  
+# by the Free Software Foundation, or (at your option) any later version.
 # This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
-# version 2 for more details.  You should have received a copy of the GNU 
-# General Public License version 2 along with this program. If not, see 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details.  You should have received a copy of the GNU
+# General Public License version 2 along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
-# $Id: Util.pm 17729 2009-04-02 01:08:21Z steve $
+
 package Tidings::Util;
 
 sub tidyfilter {
